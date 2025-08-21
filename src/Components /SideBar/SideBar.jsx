11de6ba0cb2168486b2bcbd1a255c2c46e4d2c67
deepanshu83 <dd893566039@gmail.com>
2025-08-21 22:47:38 +0530

    commit

--- src/Components /SideBar/SideBar.jsx
+++ src/Components /SideBar/SideBar.jsx
@@ -7,28 +7,28 @@ const SideBar = () => {
 
         <div className="top">
         <img className='menu block ml-2.5 cursor-pointer' src="assets/menu_icon.png" alt="" />
-        <div className="new-chat">
+        <div className="new-chat m-2.5 inline-flex items-center gap-2.5 py-2.5 px-3.5 bg-[#e6eaf1] rounded-[50px] text-xs text-gray-500 cursor-pointer">
           <img src="assets/plus_icon.png" alt="" />
           <p>New Chat</p>
         </div>
-        <div className="recent">
-          <p>Recent</p>
-          <div className="recententry">
+        <div className="recent flex flex-col ">
+          <p className='mt-7 mb-5'>Recent</p>
+          <div className="recententry flex items-start gap-2.5 p-2.5 pr-10 rounded-[50px] text-[#282828] cursor-pointer ">
             <img src="assets/message_icon.png" alt="" />
             <p>What is React....</p>
           </div>
         </div>
         </div>
         <div className="bottom">
-          <div>
+          <div className=' recententry flex items-start gap-2.5 p-2.5 pr-10 rounded-[50px] text-[#282828] cursor-pointer '>
             <img src="assets/question_icon.png" alt="" />
             <p>Help!</p>
           </div>
-          <div>
+          <div className=' recententry flex items-start gap-2.5 p-2.5 pr-10 rounded-[50px] text-[#282828] cursor-pointer '>
             <img src="assets/history_icon.png" alt="" />
             <p>Activity</p>
           </div>
-          <div>
+          <div className=' recententry flex items-start gap-2.5 p-2.5 pr-10 rounded-[50px] text-[#282828] cursor-pointer '>
             <img src="assets/setting_icon.png" alt="" />
             <p>Settings</p>
           </div>
